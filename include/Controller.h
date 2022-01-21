2e3611d072012f09b3560627f3cda955433e2ba5
Sara Zeitlin <88738433+SaraZaitlin@users.noreply.github.com>
2022-01-21 09:26:59 +0200

    Update Controller.h

--- include/Controller.h
+++ include/Controller.h
@@ -10,14 +10,15 @@ public:
 	Controller(std::unique_ptr<Command> c1, std::unique_ptr<Command> c2, std::unique_ptr<Command> c3, std::unique_ptr<Command> c4);
 
 	~Controller();
+	void run();
+private:
 	void draw();
 	void drawMenuCar();
 	void drawMenuSong();
 	int bouttonPressed(int buttonNum, sf::Vector2f position);
-	void run();
 	sf::Vector2f hendlMouse();
 	sf::Sprite setTextur(std::string name, int index)const;
-private:
+	
 	sf::Vector2f m_location;
 	sf::RenderWindow m_window;
 	sf::Sprite m_sprite;
